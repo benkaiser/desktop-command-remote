16f7ba1252e263bd77dbf12f3e29f004dd8e2154
Carlos Daniel Rocha <dan.rocha@ixpan.com.mx>
2013-11-09 15:37:40 -0600

    Mac OS commands commented. Ubuntu commands working now.

--- configure.js
+++ configure.js
@@ -3,10 +3,10 @@ cntr = 0;
 module.exports = function(){
   // ***** ONLY EDIT AFTER HERE **** //////
 
-  /* Mac OS */ addCommand("slider", "Volume", 'osascript -e "set volume output volume %s --100%"', {
+  /* Mac OS */ // addCommand("slider", "Volume", 'osascript -e "set volume output volume %s --100%"', {
   // volume control with amixer, using slider
   // requires all options. %s in command is where the number goes
-  /* Ubuntu */ // addCommand("slider", "Volume", "amixer sset Master %s%", {
+  /* Ubuntu */ addCommand("slider", "Volume", "amixer sset Master %s%", {
     min: 0,
     max: 100,
     value: 0,
@@ -14,32 +14,32 @@ module.exports = function(){
   });
 
   // send display to sleep
-  // addCommand("button", "Screen Sleep", "xset dpms force off");
-  // addCommand("button", "Screen Wake", "xset dpms force on;xset s reset;");
+  addCommand("button", "Screen Sleep", "xset dpms force off");
+  addCommand("button", "Screen Wake", "xset dpms force on;xset s reset;");
 
   // start screensaver
-  /* Mac OS */ addCommand("button", "Screensaver", "open -a /System/Library/Frameworks/ScreenSaver.framework//Versions/A/Resources/ScreenSaverEngine.app");
-  /* Ubuntu */ // addCommand("button", "Screensaver", "xscreensaver-command -activate");
+  /* Mac OS */ // addCommand("button", "Screensaver", "open -a /System/Library/Frameworks/ScreenSaver.framework//Versions/A/Resources/ScreenSaverEngine.app");
+  /* Ubuntu */ addCommand("button", "Screensaver", "xscreensaver-command -activate");
 
   // suspend system (note the confirm option, to make sure the user does not accidentally press the button)
-  /* Mac OS */ addCommand("button", "Suspend System", "pmset sleepnow", {
-  /* Ubuntu */ // addCommand("button", "Suspend System", "systemctl suspend", {
+  /* Mac OS */ // addCommand("button", "Suspend System", "pmset sleepnow", {
+  /* Ubuntu */ addCommand("button", "Suspend System", "systemctl suspend", {
       buttonClass: "btn-danger",
       confirm: true
     });
 
 // volume with buttons
-/* Mac OS */ addCommand("button", "Volume Up 2%", 'osascript -e "set volume output volume (output volume of (get volume settings) + 2) --100%"');
-/* Ubuntu */ // addCommand("button", "Volume Up 2%", "amixer sset Master 2%+");
-/* Mac OS */ addCommand("button", "Volume Down 2%", 'osascript -e "set volume output volume (output volume of (get volume settings) - 2) --100%"');
-/* Ubuntu */ // addCommand("button", "Volume Down 2%", "amixer sset Master 2%-");
-/* Mac OS */ addCommand("button", "Volume Up 10%", 'osascript -e "set volume output volume (output volume of (get volume settings) + 10) --100%"');
-/* Ubuntu */ // addCommand("button", "Volume Up 10%", "amixer sset Master 10%+");
-/* Mac OS */ addCommand("button", "Volume Down 10%", 'osascript -e "set volume output volume (output volume of (get volume settings) - 10) --100%"');
-/* Ubuntu */ // addCommand("button", "Volume Down 10%", "amixer sset Master 10%-");
+/* Mac OS */ // addCommand("button", "Volume Up 2%", 'osascript -e "set volume output volume (output volume of (get volume settings) + 2) --100%"');
+/* Ubuntu */ addCommand("button", "Volume Up 2%", "amixer sset Master 2%+");
+/* Mac OS */ // addCommand("button", "Volume Down 2%", 'osascript -e "set volume output volume (output volume of (get volume settings) - 2) --100%"');
+/* Ubuntu */ addCommand("button", "Volume Down 2%", "amixer sset Master 2%-");
+/* Mac OS */ // addCommand("button", "Volume Up 10%", 'osascript -e "set volume output volume (output volume of (get volume settings) + 10) --100%"');
+/* Ubuntu */ addCommand("button", "Volume Up 10%", "amixer sset Master 10%+");
+/* Mac OS */ // addCommand("button", "Volume Down 10%", 'osascript -e "set volume output volume (output volume of (get volume settings) - 10) --100%"');
+/* Ubuntu */ addCommand("button", "Volume Down 10%", "amixer sset Master 10%-");
 
 /* Mac OS */ // needed if want the slider to update with volume
-addCommand("button", "Get Volume", 'osascript -e  "output volume of (get volume settings)"');
+ // addCommand("button", "Get Volume", 'osascript -e  "output volume of (get volume settings)"');
 
   // ***** AND BEFORE HERE ***** ////
 
